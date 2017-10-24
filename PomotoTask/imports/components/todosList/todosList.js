fca72b11877dc89d9c22289f38dc2d151b2fbe1d
王剑虹 <smileking67@gmail.com>
2017-10-24 08:32:43 +0800

    learn data and add

--- PomotoTask/imports/components/todosList/todosList.js
+++ PomotoTask/imports/components/todosList/todosList.js
@@ -1,23 +1,34 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+
+import {Tasks} from '../../api/tasks.js'
+
 import template from './todosList.html';
 
+
 class TodosListCtrl {
-  constructor() {
-    this.tasks = [
-      {
-        text:"This is task 1"
-      },
-      {
-        text:"This is task 2"
-      },
-      {
-        text:"This is task 3"
-      },
-      {
-        text:"This is task 4"
+  constructor($scope) {
+    $scope.viewModel(this);
+
+    this.helpers({
+      tasks(){
+        return Tasks.find({}, {
+          sort: {
+            createdAt: -1
+          }
+        });
+
       }
-    ];
+    })
+  }
+
+  addTask(newTask){
+    Tasks.insert({
+      text: newTask,
+      createdAt: new Date
+    });
+
+    this.newTask = '';
   }
 }
 
@@ -25,5 +36,5 @@ export default angular.module('todosList', [
   angularMeteor
 ]).component('todosList', {
     templateUrl: 'imports/components/todosList/todosList.html',
-    controller: TodosListCtrl
+    controller: ['$scope', TodosListCtrl]
   });
